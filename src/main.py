dba120e27e633aada8211bea9b284a542c7f9495
liuaishan <liuaishan@buaa.edu.cn>
2018-05-04 23:27:58 +0800

    Add hyperparameters

--- src/main.py
+++ src/main.py
@@ -10,10 +10,10 @@ import tensorflow as tf
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 200, "epoch number to train")
 flags.DEFINE_integer("batch_size", 16, "batch size")
-flags.DEFINE_integer("traindata_size", 1e8, "train set size")
-flags.DEFINE_integer("patch_size", 28, "patch size")
+flags.DEFINE_integer("traindata_size", 1e5, "train set size")
+flags.DEFINE_integer("patch_size", 32, "patch size")
 flags.DEFINE_integer("channel", 3, "channel number")
-flags.DEFINE_integer("image_size", 256, "size of image")
+flags.DEFINE_integer("image_size", 128, "size of image")
 flags.DEFINE_float("alpha", 1.0, "parameter alpha")
 flags.DEFINE_float("beta", 1.0, "parameter beta")
 flags.DEFINE_float("gamma", 1.0, "parameter gamma")
@@ -22,9 +22,9 @@ flags.DEFINE_float("base_image_num", 4, "base number of image to augment")
 flags.DEFINE_float("base_patch_num", 4, "base number of patch to augment")
 flags.DEFINE_string("data_dir", "..\\data\\model_1\\model_best_test.ckpt", "data directory")
 flags.DEFINE_string("target_model_dir", "..\\data", "data directory")
-flags.DEFINE_string("checkpoint_dir", "../checkpoint", "directory to save model")
+flags.DEFINE_string("checkpoint_dir", "..\\checkpoint", "directory to save model")
 flags.DEFINE_string("phase", "train", "current phase of model, e.g to train or test")
-flags.DEFINE_string("output_dir", "../output", "directory to save serialized image")
+flags.DEFINE_string("output_dir", "..\\output", "directory to save serialized image")
 FLAGS = tf.app.flags.FLAGS
 
 def main(_):

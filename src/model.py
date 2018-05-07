c58a9b66471da5ebb28874b37946775ca6a5ad47
alison-fan <alison.fan@outlook.com>
2018-05-07 13:58:35 +0800

    fix the bug of "pad_patch_on_image()"

--- src/model.py
+++ src/model.py
@@ -118,9 +118,9 @@ class AdvPGAN(object):
      # pad the adversarial patch on image
     def pad_patch_on_image(self, image, patch):
         patched_image = randomly_overlay(image[0], patch[0])
-        for i in range(1, self.batch_size):
+        for i in range(1，self.batch_size):
             temp = randomly_overlay(image[i], patch[i])
-            patched_image = tf.stack([patched_image, temp])
+            patched_image = tf.concat([patched_image, temp]，0)
         return patched_image
 
         # self.patch_var = tf.Variable(tf.zeros(shape=patch.get_shape()))
